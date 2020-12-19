6f4c4fcd09b13796ff6d3d16b15235433a111033
vladislav <ve@incsub.com>
2020-12-19 17:27:38 +0100

    Simplify data structure

--- hst/hostfile.py
+++ hst/hostfile.py
@@ -8,54 +8,74 @@ def find():
 def load():
     with open(find(), "r") as fp:
         rawlines = [line.rstrip() for line in fp.readlines() if line.rstrip()]
-    lines = {}
+    return parse_lines(rawlines)
+
+def parse_lines(rawlines):
+    lines = []
+    section = "default"
     for line in rawlines:
         if line.lstrip()[0] == "#":
-            continue # comment line
+            section = uncomment(line)
+            continue
         ip, rest = line.replace(" ", "\t", 1).split("\t", 1)
-        lines[ ip ] = parse_line(rest, lines.get(ip, []))
+        lines += parse_line(ip, rest, section)
 
     return lines
 
-def parse_line(line, previous=[]):
+def parse_line(ip, line, section):
     result = line.split("#", 1)
     process = result[0].strip()
     comment = ""
     if len(result) > 1:
         comment = result[1].strip()
+    process = process.replace(" ", "\t")
 
     entries = []
     for entry in process.split("\t"):
         entries.append({
+            "ip": ip,
             "domain": entry,
             "data": parse_comment(comment),
+            "section": section,
         })
-    return previous + entries
+    return entries
 
 def parse_comment(comment):
+    timestamp = ""
+    tsstr = ""
     tsre = re.search(r'\[(\d{4}-\d{2}-\d{2}[^\]]*?)\]', comment)
-    if not tsre:
-        return { "timestamp": "", "comment": comment }
-    tsstr = tsre.group(1)
-    timestamp = f"{ dateparse(tsstr) }"
-    clean_comment = comment.replace(f"[{ tsstr }]", "").strip()
-    clean_comment = clean_comment.replace("#", "", 1).strip()
+    if tsre:
+        tsstr = tsre.group(1)
+        timestamp = f"{ dateparse(tsstr) }"
+    clean_comment = uncomment(comment.replace(f"[{ tsstr }]", ""))
     return {
         "timestamp": timestamp,
         "comment": clean_comment
     }
 
+def uncomment(line):
+    return re.sub(
+        r'^\s*#\s*',
+        '',
+        line
+    ).strip()
+
 def write(lines):
     content = []
-    for ip, entries in lines.items():
-        for entry in entries:
+    sections = set([ item.get("section") for item in lines ])
+    for section in sections:
+        content.append(f"\n# { section }")
+        for entry in [ entry for entry in lines if entry.get("section") == section ]:
+            comment = parse_data(entry.get("data"))
+            if comment:
+                comment = f"# { comment }"
             content.append(
-                f"{ ip }\t{ entry.get('domain') } # { parse_data(entry.get('data')) }"
+                f"{ entry.get('ip') }\t{ entry.get('domain') } { comment }"
             )
     print("\n".join(content))
 
 def parse_data(data):
     timestamp = data.get("timestamp")
-    if not timestamp:
-        timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-    return f"[{ timestamp }] { data.get('data') }"
+    if timestamp:
+        timestamp = f"[{ timestamp }]"
+    return f"{ timestamp } { data.get('comment') }".strip()
--- test/test_hostfile.py
+++ test/test_hostfile.py
@@ -11,7 +11,35 @@ def test_parse_comment_should_extract_existing_timestamp():
         assert res.get("comment") == comment
         assert res.get("timestamp") == expected
 
-def est_load_file_returns_lines():
+        res = hostfile.parse_comment(f"[{ timestamp }] { comment }")
+        assert res.get("comment") == comment
+        assert res.get("timestamp") == expected
+
+def test_parse_comment_should_leave_timestamp_empty_if_theres_none():
+    res = hostfile.parse_comment(f"# this is a comment")
+    assert "this is a comment" == res["comment"]
+    assert "" == res["timestamp"]
+
+def test_uncomment_strips_leading_hash_only():
+    tests = [
+        "this is a test comment",
+        "this # is a test comment",
+        "this # is # a # test comment",
+    ]
+    for expected in tests:
+       res = hostfile.uncomment(f"# { expected }")
+       assert res == expected
+
+def test_parse_lines_should_separate_space_separated_domains():
+    tests = {
+        "00.000.000.00 amherst.edu wordpress.amherst.edu": 2,
+        "00.000.000.00 amherst.edu": 1,
+    }
+    for line, expected in tests.items():
+        domains = hostfile.parse_lines([line])
+        assert expected == len(domains)
+
+def test_load_file_returns_lines():
     lines = hostfile.load()
     hostfile.write(lines)
     assert False == True
